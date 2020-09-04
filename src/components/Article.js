87794d4b4bd068a964d2de96d9e155dc3ae5a6a2
taslangraham <49318261+taslangraham@users.noreply.github.com>
2020-09-04 17:47:11 -0500

    Update Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -41,8 +41,8 @@ class Article extends Component {
 
                             <h6 className="card-title center-align">{this.props.article.headline}</h6>
 
-                            <div className="card-content" >
-                                 dangerouslySetInnerHTML={{ __html:this.props.article.description}}
+                            <div className="card-content" 
+                                 dangerouslySetInnerHTML={{ __html:this.props.article.description}}>
                                 //this.props.article.description
                             </div>
 
